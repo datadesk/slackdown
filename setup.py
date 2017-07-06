280474d9ac62506e8a251dc5b336dfff27ae9c4d
Andrew Briz <briz.andrew@gmail.com>
2017-07-06 12:55:29 -0700

    Updated pypi version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='slackdown',
-    version='0.0.1',
+    version='0.0.2',
     description='A simple Slack message text formatting to HTML code converter.',
     author='Andrew Briz',
     author_email='briz.andrew@gmail.com',
